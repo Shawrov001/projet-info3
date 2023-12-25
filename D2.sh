50b2d43e0bad0e354fbc8ec16ff6170e2650965f
Shawrov <81510878+Shawrov001@users.noreply.github.com>
2023-12-25 16:37:32 +0100

    Update D2.sh

--- D2.sh
+++ D2.sh
@@ -1,1 +1,1 @@
-sed -i 's/\./,/g' dat.csv && awk -F';' '{total[$6]+=$5} END {for (i in total) printf "%.3f;%s\n", total[i], i}' dat.csv | sort -t';' -k1nr | head -n 10
+awk -F';' '{total[$6]+=$5} END {for (i in total) printf "%.3f %s\n", total[i], i}' data.csv | sort -k1nr | head -n 10
